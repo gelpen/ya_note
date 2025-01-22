b44ec1e90307dd411cabfd526f775f51feb3ccfe
Yevgeniy_T <59078119+gelpen@users.noreply.github.com>
2025-01-22 22:37:53 +0500

    not ok

---
+++ notes/pytest_tests/test_routes.py
@@ -0,0 +1,27 @@
+
+# test_routes.py
+from django.urls import reverse
+from http import HTTPStatus
+import django
+django.setup()
+
+
+# Указываем в фикстурах встроенный клиент.
+def test_home_availability_for_anonymous_user(client):
+    # Адрес страницы получаем через reverse():
+    url = reverse('notes:home')
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+
+@pytest.mark.parametrize(
+    'name',  # Имя параметра функции.
+    # Значения, которые будут передаваться в name.
+    ('notes:home', 'users:login', 'users:logout', 'users:signup')
+)
+# Указываем имя изменяемого параметра в сигнатуре теста.
+def test_pages_availability_for_anonymous_user(client, name):
+    url = reverse(name)  # Получаем ссылку на нужный адрес.
+    response = client.get(url)  # Выполняем запрос.
+    assert response.status_code == HTTPStatus.OK
